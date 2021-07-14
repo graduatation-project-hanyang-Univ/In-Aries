acf9310025f5904383a41b31edc91002e71a7325
황태용 <xodyd15@naver.com>
2021-07-14 21:25:13 +0900

    DP-37 개발 필요 라이브러리 추가 - eslint - prettier - axios - jest

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  plugins: ['prettier'],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
