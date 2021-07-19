c57e6b5471891e60b0f9e9538a1e4c53555d5607
황태용 <xodyd15@naver.com>
2021-07-19 21:51:36 +0900

    DP-37 ACAPY api path

---
+++ services/acapy/constants.js
@@ -0,0 +1,14 @@
+const SCHEMA_PATH = {
+  ROOT: '/schemas',
+  GETS: 'created',
+};
+
+const CREDENTIAL_DEFINITION_PATH = {
+  ROOT: '/credential-definitions',
+  GETS: 'created',
+};
+
+module.exports = {
+  SCHEMA_PATH,
+  CREDENTIAL_DEFINITION_PATH,
+};
